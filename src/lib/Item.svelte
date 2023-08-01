390dfb4642fd48f521ef8657bde6d5dcaab6b950
Jakob Tormalm <jakob.tormalm@gmail.com>
2023-08-01 20:23:47 +0200

    Limit turn rate + other small fixes

--- src/lib/Item.svelte
+++ src/lib/Item.svelte
@@ -1,38 +1,52 @@
 <script lang="ts">
-    import { randomId } from '$lib/helpers'
-	import type { ItemType, ItemValues } from "./types";
-	import { onDestroy } from "svelte";
+	import { randomId } from '$lib/helpers';
+	import type { ItemType, ItemValues } from './types';
+	import { onDestroy } from 'svelte';
 
-    export let container: HTMLDivElement
-    const {width, height} = container.getBoundingClientRect()
-    const randomXPosition = () => Math.random() * (width - positionMargin) + positionMargin
-    const randomYPosition = () => Math.random() * (height - positionMargin) + positionMargin
-    const randomDirection = () => Math.random() * Math.PI * 2
-    const randomValue = (): ItemType => {
-        const random = Math.random()
-        if (random < 0.33) return 'rock'
-        if (random < 0.66) return 'paper'
-        return 'scissors'
-    }
-    const randomSpeed = () => Math.random() * 5 + 1
+	export let container: HTMLDivElement;
+	const { width, height } = container.getBoundingClientRect();
+	const randomXPosition = () => Math.random() * (width - positionMargin) + positionMargin;
+	const randomYPosition = () => Math.random() * (height - positionMargin) + positionMargin;
+	const randomDirection = () => Math.random() * Math.PI * 2;
+	const randomValue = (): ItemType => {
+		const random = Math.random();
+		if (random < 0.33) return 'rock';
+		if (random < 0.66) return 'paper';
+		return 'scissors';
+	};
+	const randomSpeed = () => Math.random() * 1.3 + 1;
 
-    export let values: ItemValues = {id: randomId(), x: randomXPosition(), y: randomYPosition(), direction: randomDirection(), value: randomValue(), speed: randomSpeed(), radius: 10, zoneX: 0, zoneY: 0 }
-    // export let items: ItemValues[] = []
+	export let values: ItemValues = {
+		id: randomId(),
+		x: randomXPosition(),
+		y: randomYPosition(),
+		direction: randomDirection(),
+		value: randomValue(),
+		speed: randomSpeed(),
+		radius: 10,
+		zoneX: 0,
+		zoneY: 0
+	};
+	// export let items: ItemValues[] = []
 
-    const positionMargin = 64
+	const positionMargin = 64;
 
-    // items.push(values)
-    // onDestroy(() => {
-    //     items = items.filter(item => item.id !== values.id)
-    // })
+	// items.push(values)
+	// onDestroy(() => {
+	//     items = items.filter(item => item.id !== values.id)
+	// })
 </script>
 
-<div class="flex items-center justify-center text-2xl absolute " style="transform: translate({values.x - values.radius}px, {values.y - values.radius}px); height: {values.radius * 2}px; width: {values.radius * 2}px;">
-    {#if values.value === 'rock'}
-        🥌
-    {:else if values.value === 'paper'}
-        📄
-    {:else if values.value === 'scissors'}
-        ✂️
-    {/if}
+<div
+	class="flex items-center justify-center text-2xl absolute select-none"
+	style="transform: translate({values.x - values.radius}px, {values.y -
+		values.radius}px); height: {values.radius * 2}px; width: {values.radius * 2}px;"
+>
+	{#if values.value === 'rock'}
+		🥌
+	{:else if values.value === 'paper'}
+		📄
+	{:else if values.value === 'scissors'}
+		✂️
+	{/if}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,15 @@
 	import { onDestroy, onMount } from 'svelte';
 	import { construct_svelte_component } from 'svelte/internal';
 	import { writable } from 'svelte/store';
-
+	import { fade } from 'svelte/transition';
+	import { cubicInOut } from 'svelte/easing';
 
 	let container: HTMLDivElement;
 	let items: ItemValues[] = [];
 
-	const verticalZones = 64;
-	const horizonalZones = 36;
+	const verticalZones = 32;
+	const horizonalZones = 18;
+	const itemCount = 150;
 
 	// create the zones
 	const zones: string[][][] = [];
@@ -22,162 +24,178 @@
 				zones[i][j] = [];
 			}
 		}
-	}
+	};
 
 	refreshZones();
 	let width: number, height: number;
 
-	const itemCount = 450;
-
 	type TargetEnemies = {
-		[key: string]: ItemValues
-	}
-	const currentTargets: TargetEnemies = {}
+		[key: string]: ItemValues;
+	};
+	const currentTargets: TargetEnemies = {};
 
 	const checkWallCollision = (item: ItemValues) => {
-        if (!container) return
+		if (!container) return;
 		// const { width, height } = container.getBoundingClientRect();
+		let collided = false;
 		if (item.x + item.radius > width || item.x - item.radius < 0) {
 			item.direction = Math.PI - item.direction;
-			item.value = getNextValue(item.value);
+			collided = true;
 		}
 		if (item.y + item.radius > height || item.y - item.radius < 0) {
 			item.direction = -item.direction;
-			item.value = getNextValue(item.value);
+			collided = true;
 		}
-	}
+		if (collided) {
+			// item.value = getNextValue(item.value);
+		}
+	};
 
 	const getEnemyType = (value: ItemType) => {
-		if (value === 'rock') return 'scissors'
-		if (value === 'paper') return 'rock'
-		return 'paper'
-	}
+		if (value === 'rock') return 'scissors';
+		if (value === 'paper') return 'rock';
+		return 'paper';
+	};
 
 	const switchValue = (item1: ItemValues, item2: ItemValues) => {
-
-		const item1Value = item1.value
-		const item2Value = item2.value
+		const item1Value = item1.value;
+		const item2Value = item2.value;
 
 		if (item1Value === 'rock') {
 			if (item2Value === 'paper') {
-				item1.value = 'paper'
+				item1.value = 'paper';
 			} else if (item2Value === 'scissors') {
-				item2.value = 'rock'
+				item2.value = 'rock';
 			}
 		} else if (item1Value === 'paper') {
 			if (item2Value === 'rock') {
-				item2.value = 'paper'
+				item2.value = 'paper';
 			} else if (item2Value === 'scissors') {
-				item1.value = 'scissors'
+				item1.value = 'scissors';
 			}
 		} else if (item1Value === 'scissors') {
 			if (item2Value === 'rock') {
-				item1.value = 'rock'
+				item1.value = 'rock';
 			} else if (item2Value === 'paper') {
-				item2.value = 'scissors'
+				item2.value = 'scissors';
 			}
 		}
-	}
+	};
 
 	const checkItemCollisions = (item: ItemValues) => {
 		for (const otherItem of items) {
-			if (otherItem.id === item.id) continue
-			if (Math.abs(item.zoneX - otherItem.zoneX) > 1 || Math.abs(item.zoneY - otherItem.zoneY) > 1) continue
+			if (otherItem.id === item.id) continue;
+			if (Math.abs(item.zoneX - otherItem.zoneX) > 1 || Math.abs(item.zoneY - otherItem.zoneY) > 1)
+				continue;
 			const dx = item.x - otherItem.x;
 			const dy = item.y - otherItem.y;
-			if (dx > item.radius + otherItem.radius || dy > item.radius + otherItem.radius) continue
+			if (dx > item.radius + otherItem.radius || dy > item.radius + otherItem.radius) continue;
 			const distance = Math.sqrt(dx * dx + dy * dy);
 			if (distance < item.radius + otherItem.radius) {
 				// $items[itemId].direction = Math.atan2(dy, dx);
 				// $items[otherItemId].direction = Math.atan2(-dy, -dx);
-				switchValue(item, otherItem)
+				switchValue(item, otherItem);
 			}
 		}
-	}
+	};
 
 	const setZone = (item: ItemValues) => {
-		item.zoneY = Math.floor(item.y / height * verticalZones);
-		item.zoneX = Math.floor(item.x / width * horizonalZones);
-		
-	}
+		item.zoneY = Math.floor((item.y / height) * verticalZones);
+		item.zoneX = Math.floor((item.x / width) * horizonalZones);
+	};
+
+	const maxTurn = 0.04;
+	const setNewDirection = (item: ItemValues, direction: number) => {
+		if (Math.abs(item.direction - direction) > maxTurn) {
+			if (item.direction > direction) {
+				item.direction -= maxTurn;
+			} else {
+				item.direction += maxTurn;
+			}
+		} else {
+			item.direction = direction;
+		}
+	};
 
 	const setHomingDirection = (item: ItemValues) => {
-		if (hasWon) return
-		const targetEnemyType = getEnemyType(item.value)
+		if (hasWon) return;
+		const targetEnemyType = getEnemyType(item.value);
 
 		if (currentTargets[item.id]) {
-			const targetEnemy = currentTargets[item.id]
-			if (!targetEnemy || targetEnemy.value !== targetEnemyType) {
-				delete currentTargets[item.id]
+			const targetEnemy = currentTargets[item.id];
+			if (targetEnemy.value !== targetEnemyType) {
+				delete currentTargets[item.id];
 			} else {
-				item.direction = Math.atan2(targetEnemy.y - item.y, targetEnemy.x - item.x);
-				return
+				// item.direction = Math.atan2(targetEnemy.y - item.y, targetEnemy.x - item.x);
+				setNewDirection(item, Math.atan2(targetEnemy.y - item.y, targetEnemy.x - item.x));
+				return;
 			}
 		}
-		
-		let closestEnemy: ItemValues | null = null
-		let closestDistance = Infinity
+
+		let closestEnemy: ItemValues | null = null;
+		let closestDistance = Infinity;
+
 		for (const otherItem of items) {
-			if (otherItem.id === item.id) continue
-			if (otherItem.value !== targetEnemyType) continue
+			if (otherItem.id === item.id) continue;
+			if (otherItem.value !== targetEnemyType) continue;
 			const dx = item.x - otherItem.x;
 			const dy = item.y - otherItem.y;
 			const distance = Math.sqrt(dx * dx + dy * dy);
 			if (distance < closestDistance) {
-				closestDistance = distance
-				closestEnemy = otherItem
+				closestDistance = distance;
+				closestEnemy = otherItem;
 				// break
 			}
 		}
 
 		if (closestEnemy) {
-			item.direction = Math.atan2(closestEnemy.y - item.y, closestEnemy.x - item.x);
-			currentTargets[item.id] = closestEnemy
+			// item.direction = Math.atan2(closestEnemy.y - item.y, closestEnemy.x - item.x);
+			setNewDirection(item, Math.atan2(closestEnemy.y - item.y, closestEnemy.x - item.x));
+			currentTargets[item.id] = closestEnemy;
 		}
-
-	}
+	};
 
 	const moveItem = (item: ItemValues) => {
-		setHomingDirection(item)
+		setHomingDirection(item);
 		item.x += Math.cos(item.direction) * item.speed;
 		item.y += Math.sin(item.direction) * item.speed;
-		setZone(item)
+		setZone(item);
 		checkWallCollision(item);
 		checkItemCollisions(item);
-	}
+	};
 
 	const calculateCounts = () => {
-		scissorsCount = 0
-		rockCount = 0
-		paperCount = 0
+		scissorsCount = 0;
+		rockCount = 0;
+		paperCount = 0;
 		for (const item of items) {
-			if (item.value === 'scissors') scissorsCount++
-			if (item.value === 'rock') rockCount++
-			if (item.value === 'paper') paperCount++
+			if (item.value === 'scissors') scissorsCount++;
+			if (item.value === 'rock') rockCount++;
+			if (item.value === 'paper') paperCount++;
 		}
-	}
+	};
 
 	const getNextValue = (value: ItemType) => {
-		if (value === 'rock') return 'paper'
-		if (value === 'paper') return 'scissors'
-		return 'rock'
-	}
+		if (value === 'rock') return 'paper';
+		if (value === 'paper') return 'scissors';
+		return 'rock';
+	};
 
 	const changeValue = (item: ItemValues) => {
 		if (Math.random() < 0.0005) {
-			item.value = getNextValue(item.value)
+			item.value = getNextValue(item.value);
 		}
-	}
+	};
 
 	const run = () => {
-		if (!container) return
-		const rect = container.getBoundingClientRect()
-		width = rect.width
-		height = rect.height
+		if (!container) return;
+		const rect = container.getBoundingClientRect();
+		width = rect.width;
+		height = rect.height;
 		for (const item of items) {
-			moveItem(item)
-			const index = items.indexOf(item)
-			items[index] = item
+			moveItem(item);
+			const index = items.indexOf(item);
+			items[index] = item;
 			// if (!hasWon) {
 			// 	changeValue(itemId)
 			// }
@@ -185,76 +203,73 @@
 
 		// do a for loop using the index so we can modify the array
 
-		calculateCounts()
-	}
+		calculateCounts();
+	};
 
 	const runCountdown = () => {
-		if (!hasWon) return
+		if (!hasWon) return;
 		if (countdown > 0) {
-			countdown--
+			countdown--;
 		} else {
-			countdown = 5
-			restartGame = !restartGame
-			items = []
+			countdown = 5;
+			restartGame = !restartGame;
+			items = [];
 		}
-	}
-	
-	let frametime = 0
-	let frametimes: number[] = []
+	};
 
-	let countdownInterval: number
-	let frametimeSorterInterval: number
+	let frametime = 0;
+	let frametimes: number[] = [];
+
+	let countdownInterval: number;
+	let frametimeSorterInterval: number;
 	onMount(async () => {
 		// const interval = setInterval(run, 1000 / 60)
-		countdownInterval = setInterval(runCountdown, 1000)
+		countdownInterval = setInterval(runCountdown, 1000);
 		frametimeSorterInterval = setInterval(() => {
-			frametimes.sort((a, b) => a - b)
-			frametimes = frametimes
-		}, 1000)
+			frametimes.sort((a, b) => a - b);
+			frametimes = frametimes;
+		}, 1000);
 		while (true) {
-			const now = performance.now()
-			run()
-			frametime = performance.now() - now
-			frametimes.push(frametime)
-			const timeToWait = 1000 / 144 - frametime
-			await new Promise(resolve => setTimeout(resolve, timeToWait > 0 ? timeToWait : 0.1))
+			const now = performance.now();
+			run();
+			frametime = performance.now() - now;
+			frametimes.push(frametime);
+			const timeToWait = 1000 / 60 - frametime;
+			await new Promise((resolve) => setTimeout(resolve, timeToWait > 0 ? timeToWait : 0.1));
 		}
-		
+
 		// onDestroy(() => clearInterval(interval))
-		
-	})
-	onDestroy(() => clearInterval(countdownInterval))
-	onDestroy(() => clearInterval(frametimeSorterInterval))
+	});
+	onDestroy(() => clearInterval(countdownInterval));
+	onDestroy(() => clearInterval(frametimeSorterInterval));
 
-	let scissorsCount = 0
-	let rockCount = 0
-	let paperCount = 0
+	let scissorsCount = 0;
+	let rockCount = 0;
+	let paperCount = 0;
 
-	let hasWon = false
-	let countdown = 5
+	let hasWon = false;
+	let countdown = 5;
 
-	$: hasWon = scissorsCount === itemCount || rockCount === itemCount || paperCount === itemCount
-	
-	$: hasWon, resetCountdown()
-	let restartGame = false
-	$: restartGame, items = []
+	$: hasWon = scissorsCount === itemCount || rockCount === itemCount || paperCount === itemCount;
+
+	$: hasWon, resetCountdown();
+	let restartGame = false;
+	$: restartGame, (items = []);
 
 	const resetCountdown = () => {
 		if (!hasWon) {
-			countdown = 5
+			countdown = 5;
 		}
-	}
-	
+	};
 </script>
 
-
-<div class="w-full h-full flex items-center justify-center">
+<div class="w-full h-full flex items-center justify-center overflow-hidden">
 	<div class="h-full w-full p-4 rounded-md bg-surface-800">
 		<div class="w-full h-full relative" bind:this={container}>
 			{#key restartGame}
 				{#if container}
 					{#each Array(itemCount) as _, index}
-						<Item {container} bind:values={items[index]}/>
+						<Item {container} bind:values={items[index]} />
 					{/each}
 				{/if}
 			{/key}
@@ -266,7 +281,7 @@
 	<span class="py-2 px-4 variant-soft-primary rounded-md">
 		✂️ {scissorsCount}
 	</span>
-	<span class="py-2 px-4 variant-soft-primary rounded-md">	
+	<span class="py-2 px-4 variant-soft-primary rounded-md">
 		🥌 {rockCount}
 	</span>
 	<span class="py-2 px-4 variant-soft-primary rounded-md">
@@ -274,21 +289,35 @@
 	</span>
 </div>
 
-<button class="button absolute top-4 right-4 text-4xl py-2 px-4 variant-glass-primary rounded-md" on:click={() => restartGame = !restartGame}>
+<button
+	class="button absolute top-4 right-4 text-4xl py-2 px-4 variant-glass-primary rounded-md"
+	on:click={() => (restartGame = !restartGame)}
+>
 	🔄
 </button>
 
-<span class="absolute top-4 left-4 text-4xl py-2 px-4 variant-glass-primary rounded-md" >
-	ms: {Math.round(frametime)} <br>
-	frames: {frametimes.length || 0} <br>
-	avg: {Math.round(frametimes.reduce((a, b) => a + b, 0) / frametimes.length) || 0} <br>
-	worst: {Math.round(frametimes[frametimes.length - 1]) || 0} <br>
-	1%: {Math.round(frametimes.slice(frametimes.length - Math.round(frametimes.length * 0.01)).reduce((a, b) => a + b, 0) / Math.round(frametimes.length * 0.01)) || 0} <br>
-	0.1%: {Math.round(frametimes.slice(frametimes.length - Math.round(frametimes.length * 0.001)).reduce((a, b) => a + b, 0) / Math.round(frametimes.length * 0.001)) || 0} 
+<span class="absolute top-4 left-4 text-4xl py-2 px-4 variant-glass-primary rounded-md">
+	ms: {Math.round(frametime)} <br />
+	frames: {frametimes.length || 0} <br />
+	avg: {Math.round(frametimes.reduce((a, b) => a + b, 0) / frametimes.length) || 0} <br />
+	worst: {Math.round(frametimes[frametimes.length - 1]) || 0} <br />
+	1%: {Math.round(
+		frametimes
+			.slice(frametimes.length - Math.round(frametimes.length * 0.01))
+			.reduce((a, b) => a + b, 0) / Math.round(frametimes.length * 0.01)
+	) || 0} <br />
+	0.1%: {Math.round(
+		frametimes
+			.slice(frametimes.length - Math.round(frametimes.length * 0.001))
+			.reduce((a, b) => a + b, 0) / Math.round(frametimes.length * 0.001)
+	) || 0}
 </span>
 
 {#if hasWon}
-	<span class="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-6xl px-4 py-2 bg-black bg-opacity-50 rounded-md text-center">
+	<span
+		transition:fade={{ duration: 250, easing: cubicInOut }}
+		class="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-6xl px-4 py-2 bg-black bg-opacity-50 rounded-md text-center"
+	>
 		{#if scissorsCount === itemCount}
 			✂️
 		{:else if rockCount === itemCount}
@@ -296,11 +325,10 @@
 		{:else if paperCount === itemCount}
 			📄
 		{/if}
-		 won
-		 <br/>
-		 <span class="text-2xl">
-			restarting in {countdown === 5 ? 5 :  countdown + 1} seconds
-		 </span>
+		won
+		<br />
+		<span class="text-2xl">
+			restarting in {countdown === 5 ? 5 : countdown + 1} seconds
+		</span>
 	</span>
 {/if}
-
